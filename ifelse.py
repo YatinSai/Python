1b1f2bf81014b470befb64826bdf6febd955a008
YatinSai <71301360+YatinSai@users.noreply.github.com>
2021-08-15 19:46:06 -0400

    Add files via upload

---
+++ ifelse.py
@@ -0,0 +1,11 @@
+name = input('Enter Your Name:')
+age = int(input('Enter Your Age: '))
+
+if age > 60 : 
+    print("you are older than 60")
+elif age>18:
+    print("yau can vote")
+else:
+    print("you are too young")
+
+
